f8aac9192136646714aacad3994b3b20f1625e2e
Nikolay <kuoluska@mail.ru>
2021-05-28 02:00:25 +0900

    в основном классе добавил объявил кнопку с вызовом метода  onClick

--- app/src/main/java/ru/geekbrains/myapplication01/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication01/MainActivity.java
@@ -2,7 +2,10 @@ package ru.geekbrains.myapplication01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Button buttonRunTask2Maket = findViewById(R.id.switchAnotherLayout);
+        buttonRunTask2Maket.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
