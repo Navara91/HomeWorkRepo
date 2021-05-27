c3eb56a689945fa50aec535e2a1a14fb01c66e53
Nikolay <kuoluska@mail.ru>
2021-05-28 01:38:54 +0900

    в созданном классе для 2 задания вызвал метод onCreate

---
+++ app/src/main/java/ru/geekbrains/myapplication01/secondActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.myapplication01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class secondActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
